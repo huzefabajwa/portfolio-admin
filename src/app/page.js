8bb3badfd53d4c05b5c91f798b016fbe82601730
Huzefa Bajwa <huzefa_bajwa@hotmail.com>
2026-01-04 16:36:38 -0500

    New Changes

--- src/app/page.js
+++ src/app/page.js
@@ -16,7 +16,7 @@ const ClientServicesView = dynamic(() => import("@/components/client-view/servic
 const ClientReviewsView = dynamic(() => import("@/components/client-view/reviews"), { ssr: false });
 
 async function extractAllDatas(currentSection) {
-  const res = await fetch(`https://talha-bajwa.vercel.app/api/${currentSection}/get`, {
+  const res = await fetch(`https://talhabajwa.site/api/${currentSection}/get`, {
     method: "GET",
     cache: "no-store",
   });
